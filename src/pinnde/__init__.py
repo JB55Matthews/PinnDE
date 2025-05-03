63aa86d288cf81fd21487d0fa88a49305839412d
JB55Matthews <blackhedley25@gmail.com>
2025-05-03 16:31:23 -0230

    new base

---
+++ src/pinnde/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "boundaries",
+    "geometry",
+    "models",
+    "plotters",
+    "initials"
+]
+
+from . import boundaries
+from . import geometry
+from . import models
+from . import plotters
+
+from . import initials
